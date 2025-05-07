8d3f920c9c7393837a4acf23aa4b07a3720a130e
WitoldFa <wito9876@wp.pl>
2025-05-08 00:22:56 +0200

    Historia

--- Gry_Karciane/Form1.cs
+++ Gry_Karciane/Form1.cs
@@ -4,7 +4,7 @@ namespace Gry_Karciane
     {
         public Form1()
         {
-            InitializeComponent();
+            InitializeComponent();//Historia
         }
 
         private void Wojna_Click(object sender, EventArgs e)
